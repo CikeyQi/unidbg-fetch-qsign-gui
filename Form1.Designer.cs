6fcf234279a4a235bf68339a68b8ea9d8a4af916
苏沫柒 <3146312184@qq.com>
2023-08-04 21:43:15 +0800

    Upload v1.0.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace qsign
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            linkLabel3 = new LinkLabel();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("点字倔强黑", 14.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(116, 63);
+            comboBox1.Margin = new Padding(2, 3, 2, 3);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(187, 34);
+            comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            comboBox1.SelectedValueChanged += comboBox1_SelectedValueChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("点字倔强黑", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(11, 114);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(96, 27);
+            label1.TabIndex = 1;
+            label1.Text = "监听端口";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("点字倔强黑", 14.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(116, 114);
+            textBox1.Margin = new Padding(2, 3, 2, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(187, 34);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("点字倔强黑", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(11, 63);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(96, 27);
+            label2.TabIndex = 1;
+            label2.Text = "协议版本";
+            label2.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.CornflowerBlue;
+            button1.Font = new Font("点字倔强黑", 14.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.Transparent;
+            button1.Location = new Point(11, 223);
+            button1.Margin = new Padding(2, 3, 2, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(292, 70);
+            button1.TabIndex = 3;
+            button1.Text = "启动";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("点字倔强黑", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.ImeMode = ImeMode.NoControl;
+            textBox2.Location = new Point(317, 18);
+            textBox2.Margin = new Padding(2, 3, 2, 3);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.ScrollBars = ScrollBars.Vertical;
+            textBox2.Size = new Size(356, 275);
+            textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("点字倔强黑", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(11, 163);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(96, 27);
+            label3.TabIndex = 1;
+            label3.Text = "注册密钥";
+            label3.Click += label1_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("点字倔强黑", 14.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(116, 163);
+            textBox3.Margin = new Padding(2, 3, 2, 3);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(187, 34);
+            textBox3.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("点字倔强黑", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(107, 18);
+            label5.Margin = new Padding(2, 0, 2, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(75, 27);
+            label5.TabIndex = 1;
+            label5.Text = "未启动";
+            label5.Click += label1_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(529, 305);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(44, 17);
+            linkLabel1.TabIndex = 5;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "原项目";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Location = new Point(579, 305);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(44, 17);
+            linkLabel2.TabIndex = 5;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "本项目";
+            linkLabel2.LinkClicked += linkLabel2_LinkClicked;
+            // 
+            // linkLabel3
+            // 
+            linkLabel3.AutoSize = true;
+            linkLabel3.Location = new Point(629, 305);
+            linkLabel3.Name = "linkLabel3";
+            linkLabel3.Size = new Size(44, 17);
+            linkLabel3.TabIndex = 5;
+            linkLabel3.TabStop = true;
+            linkLabel3.Text = "交流群";
+            linkLabel3.LinkClicked += linkLabel3_LinkClicked;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(684, 330);
+            Controls.Add(linkLabel3);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Controls.Add(textBox2);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Font = new Font("Microsoft YaHei UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(2, 3, 2, 3);
+            Name = "Main";
+            Text = "unidbg-fetch-qsign-gui  v1.0.0";
+            FormClosing += Main_FormClosing;
+            Load += Main_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button1;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label5;
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+        private LinkLabel linkLabel3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,412 @@
+using System.Diagnostics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+namespace qsign
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void write_config(string config_path)
+        {
+            string json = File.ReadAllText(config_path);
+            JObject jo = (JObject)JsonConvert.DeserializeObject(json);
+            jo["server"]["port"] = Convert.ToInt32(textBox1.Text);
+            jo["key"] = textBox3.Text;
+            string json1 = jo.ToString();
+            File.WriteAllText(config_path, json1);
+
+
+        }
+        private string get_target_id(int port)
+        {
+            string output;
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = "cmd ";
+                process.StartInfo.Arguments = $"/c netstat -ano | findstr \"{port}\"";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+                output = process.StandardOutput.ReadToEnd();
+            }
+            System.Diagnostics.Debug.WriteLine(output);
+            string[] lines = output.Split("\n");
+            List<Dictionary<string, string>> result = new List<Dictionary<string, string>>();
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length > 4)
+                {
+                    result.Add(new Dictionary<string, string>(){
+                              {"pid", parts[4]},
+                              {"address", parts[1]},
+                              {"state", parts[3]}
+                            });
+                }
+            }
+            string targetPid = "";
+            foreach (var item in result)
+            {
+                if (item["state"] == "LISTENING")
+                {
+                    targetPid = item["pid"];
+                    break;
+                }
+            }
+            return targetPid;
+        }
+        private void check()
+        {
+            while (flag)
+            {
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://127.0.0.1:" + port.ToString());
+                    request.Method = "GET";
+                    HttpWebResponse httpWebResponse = (HttpWebResponse)request.GetResponse();
+                    if (((int)httpWebResponse.StatusCode) == 200)
+                    {
+                        checknum += 1;
+
+                        label5.Invoke((Action)(() =>
+                        {
+                            label5.Text = "正在运行";
+                            label5.ForeColor = Color.Green;
+                            if (checknum == 1)
+                            {
+                                if (MessageBox.Show("http://127.0.0.1:" + port.ToString() + "/sign?key=" + key + "\n点击确定复制Sign地址", "启动成功", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+                                {
+                                    Clipboard.SetDataObject("http://127.0.0.1:" + port.ToString() + "/sign?key=" + key);
+                                }
+                            }
+                        }));
+
+                    }
+                    else
+                    {
+                        label5.Invoke((Action)(() =>
+                        {
+                            label5.Text = "状态异常 " + httpWebResponse.StatusCode.ToString();
+                            label5.ForeColor = Color.Red;
+                        }));
+
+                    }
+                    httpWebResponse.Close();
+                }
+                catch
+                {
+                    label5.Invoke((Action)(() =>
+                    {
+                        label5.Text = "状态异常";
+                        label5.ForeColor = Color.Red;
+                    }));
+                }
+                try
+                {
+                    Thread.Sleep(1000);
+                }
+                catch
+                {
+
+                }
+
+            }
+
+
+        }
+        Thread newThread;
+        Thread CheckThread;
+        string verison;
+        int port;
+        string key;
+        bool flag = false;
+        int checknum = 0;
+        [Obsolete]
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "启动")
+            {
+                string currentDirectory = Environment.CurrentDirectory;
+                bool exist = File.Exists(currentDirectory + "\\bin\\unidbg-fetch-qsign.bat");
+                if (exist == false)
+                {
+                    MessageBox.Show("启动脚本不存在", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                string path = currentDirectory + "\\txlib";
+                string[] folders = Directory.GetDirectories(path);
+                List<string> versions = new List<string>();
+                foreach (string folder in folders)
+                {
+                    versions.Add(folder.Replace(currentDirectory + "\\txlib\\", ""));
+                    System.Diagnostics.Debug.WriteLine(folder.Replace(currentDirectory + "\\txlib\\", ""));
+                }
+                if (!versions.Contains<string>(comboBox1.Text))
+                {
+                    MessageBox.Show("版本不存在", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (textBox1.Text == "" || textBox3.Text == "")
+                {
+                    MessageBox.Show("请输入相应内容", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                try
+                {
+                    port = Convert.ToInt32(textBox1.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("端口错误", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (get_target_id(port) != "")
+                {
+                    MessageBox.Show("端口被占用，请更换端口号再试", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                int[] banned_port = { 1, 7, 9, 11, 13, 15, 17, 19, 20, 21, 22, 23, 25, 37, 42, 43, 53, 77, 79, 87, 95, 101, 102, 103, 104, 109, 110, 111, 113, 115, 117, 119, 123, 135, 139, 143, 179, 389, 465, 512, 513, 514, 515, 526, 530, 531, 532, 540, 556, 563, 587, 601, 636, 993, 995, 2049, 3659, 4045, 6000, 6665, 6666, 6667, 6668, 6669 };
+                if (banned_port.Contains<int>(port))
+                {
+                    MessageBox.Show("非安全端口，请更换端口号再试", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                verison = comboBox1.Text;
+                write_config(currentDirectory + "\\txlib\\" + comboBox1.Text + "\\config.json");
+                flag = true;
+                checknum = 0;
+                newThread = new Thread(new ThreadStart(NewThread));
+                CheckThread = new Thread(new ThreadStart(check));
+                button1.Text = "停止";
+                newThread.IsBackground = true;
+                newThread.Start();
+                CheckThread.IsBackground = true;
+                CheckThread.Start();
+            }
+            else
+            {
+                flag = false;
+                if (newThread.IsAlive)
+                {
+
+                    string targetPid = get_target_id(port);
+                    if (targetPid != "")
+                    {
+                        System.Diagnostics.Debug.WriteLine(targetPid);
+                        var fileName = "cmd";
+                        var arguments = "/c taskkill /F /PID " + targetPid;
+
+                        // 创建进程启动信息
+                        var processStartInfo = new ProcessStartInfo()
+                        {
+                            FileName = fileName,
+                            Arguments = arguments,
+                            RedirectStandardOutput = true,
+                            CreateNoWindow = true,
+                            UseShellExecute = false
+                        };
+
+                        // 执行命令
+                        var process1 = Process.Start(processStartInfo);
+                        process1.WaitForExit();
+                        while (newThread.IsAlive)
+                        {
+                            Thread.Sleep(100);
+                        }
+                    }
+                    CheckThread.Interrupt();
+                    button1.Text = "启动";
+                    label5.Text = "未启动";
+                    label5.ForeColor = Color.Black;
+                    textBox2.AppendText("\nServer已停止");
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+        private void NewThread()
+        {
+            RunCmd();
+        }
+
+        private void RunCmd()
+        {
+            string currentDirectory = Environment.CurrentDirectory;
+            Process process = new Process();
+            process.StartInfo.UseShellExecute = false; //必要参数
+            process.StartInfo.RedirectStandardOutput = true;//输出参数设定
+            process.StartInfo.RedirectStandardInput = true;//传入参数设定
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.FileName = "cmd.exe";
+            process.Start();
+            process.StandardInput.WriteLine("call bin\\unidbg-fetch-qsign.bat --basePath=txlib\\" + verison);
+            //process.StandardInput.AutoFlush = true;
+            while (!process.StandardOutput.EndOfStream & flag)
+            {
+                string t = "";
+                try
+                {
+                    t = process.StandardOutput.ReadLine();
+                }
+                catch
+                {
+                    continue;
+                }
+                finally
+                {
+                    textBox2.Invoke((Action)(() =>
+                    {
+                        textBox2.AppendText(t + "\n");
+                    }));
+
+                }
+
+
+            }
+            process.Kill();
+
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            string currentDirectory = Environment.CurrentDirectory;
+            string path = currentDirectory + "\\txlib";
+            string[] folders = Directory.GetDirectories(path);
+
+            foreach (string folder in folders)
+            {
+                comboBox1.Items.Add(folder.Replace(currentDirectory + "\\txlib\\", ""));
+            }
+
+
+
+
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+            string currentDirectory = Environment.CurrentDirectory;
+            string config_path = currentDirectory + "\\txlib\\" + comboBox1.Text + "\\config.json";
+            if (!File.Exists(config_path))
+            {
+                MessageBox.Show("...");
+                return;
+            }
+            string json = File.ReadAllText(config_path);
+            JObject jo = (JObject)JsonConvert.DeserializeObject(json);
+            port = (int)jo["server"]["port"];
+            key = (string)jo["key"];
+            textBox1.Text = port.ToString();
+            textBox3.Text = key;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (flag)
+            {
+                if (MessageBox.Show("Server仍在运行，是否关闭窗口", "是否关闭", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    flag = false;
+                    if (newThread.IsAlive)
+                    {
+
+                        string targetPid = get_target_id(port);
+                        if (targetPid != "")
+                        {
+                            System.Diagnostics.Debug.WriteLine(targetPid);
+                            var fileName = "cmd";
+                            var arguments = "/c taskkill /F /PID " + targetPid;
+
+                            // 创建进程启动信息
+                            var processStartInfo = new ProcessStartInfo()
+                            {
+                                FileName = fileName,
+                                Arguments = arguments,
+                                RedirectStandardOutput = true,
+                                CreateNoWindow = true,
+                                UseShellExecute = false
+                            };
+
+                            // 执行命令
+                            var process1 = Process.Start(processStartInfo);
+                            process1.WaitForExit();
+                            while (newThread.IsAlive)
+                            {
+                                Thread.Sleep(100);
+                            }
+                        }
+                        CheckThread.Interrupt();
+                        button1.Text = "启动";
+                        label5.Text = "未启动";
+                        label5.ForeColor = Color.Black;
+                        textBox2.AppendText("\nServer已停止");
+                    }
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "https://github.com/fuqiuluo/unidbg-fetch-qsign",
+                UseShellExecute = true
+            });
+            //System.Diagnostics.Process.Start("https://github.com/fuqiuluo/unidbg-fetch-qsign");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "https://github.com/CikeyQi/unidbg-fetch-qsign",
+                UseShellExecute = true
+            });
+            // System.Diagnostics.Process.Start("https://github.com/CikeyQi/unidbg-fetch-qsign");
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "https://jq.qq.com/?_wv=1027&k=FZUabhdf",
+                UseShellExecute = true
+            });
+            //System.Diagnostics.Process.Start("https://jq.qq.com/?_wv=1027&k=FZUabhdf");
+        }
+    }
+}
